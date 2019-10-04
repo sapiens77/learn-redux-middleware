50189b094a00d600299b2035152b54ff7adb60c9
sapeins77 <sapiens.hwan@gmail.com>
2019-10-04 14:43:44 +0900

    2

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -14,4 +14,102 @@ const GET_USER_FAILURE = "sample/GET_USER_FAILURE";
 // thunk 함수를 생성한다.
 // thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치한다.
 
-export const getPost = id => async dispatch => {};
+export const getPost = id => async dispatch => {
+  dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
+  try {
+    const response = await api.getPost(id);
+    dispatch({
+      type: GET_POST_SUCCESS,
+      payload: response.data
+    }); //요청 성공
+  } catch (e) {
+    dispatch({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true
+    });
+    throw e; // 나중에 컴포넌트 단에서 에러를 조회하 수 있게 해줌
+  }
+};
+
+export const getUsers = () => async dispatch => {
+  dispatch({ type: GET_USERS }); // 요청을 시작한 것을 알림
+  try {
+    const response = await api.getUser();
+    dispatch({
+      type: GET_USERS_SUCCESS,
+      payload: response.data
+    }); //요청 성공
+  } catch (e) {
+    dispatch({
+      type: GET_USERS_FAILURE,
+      payload: e,
+      error: true
+    });
+    throw e; // 나중에 컴포넌트 단에서 에러를 조회하 수 있게 해줌
+  }
+};
+
+// 초기 상태를 선언한다.
+// 요청의 로딩 중 상태는 loading이라는 객체에서 관리한다.
+
+const initialState = {
+  loading: {
+    GET_POST: false,
+    GET_USERS: false
+  },
+  post: null,
+  users: null
+};
+
+const sample = handleActions(
+  {
+    [GET_POST]: state => ({
+      ...state,
+      loading: {
+        ...state.loadig,
+        GET_POST: true //요청 시작
+      }
+    }),
+    [GET_POST_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loadig,
+        GET_POST: true //요청 완료
+      },
+      post: action.payload
+    }),
+    [GET_POST_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loadig,
+        GET_POST: false //요청 완료
+      }
+    }),
+    [GET_USERS]: state => ({
+      ...state,
+      loading: {
+        ...state.loadig,
+        GET_USERS: true //요청 시작
+      }
+    }),
+    [GET_USERS_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loadig,
+        GET_USERS: true //요청 완료
+      },
+      users: action.payload
+    }),
+    [GET_USERS_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loadig,
+        GET_USERS: false //요청 완료
+      }
+    })
+  },
+  initialState
+);
+
+export default sample;
